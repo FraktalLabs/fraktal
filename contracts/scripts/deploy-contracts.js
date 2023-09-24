c73dd7a3bd1ca5a0859bbc98bc91f806df9ffec4
Brandon Roberts <brandonjroberts22@gmail.com>
2023-09-24 14:45:37 -0700

    Basic contract setup to build SCs using solidity submodule fork and deploy to fraktal nodes using web3js

---
+++ contracts/scripts/deploy-contracts.js
@@ -0,0 +1,20 @@
+import { deploy } from './deploy-deps.js'
+import fs from 'fs'
+
+(async () => {
+  try {
+      console.log("Deploying contracts... w/ sequencer address: ", process.env.SEQUENCER_ADDRESS)
+      const result = await deploy('Test', [], 'http://localhost:8545')
+      console.log(result)
+      console.log("Deployed Test contract to : ", result.address)
+      var jsonOutput = "{\"address\": \"" + result.address + "\"}"
+      // Write the contract address to a file
+      fs.writeFileSync('./builds/test-address.txt', jsonOutput)
+
+  } catch (e) {
+      console.log(e.message)
+  }
+
+  process.exit(0)
+})()
+
---
+++ contracts/scripts/deploy-deps.js
@@ -0,0 +1,56 @@
+import fs from 'fs'
+import Web3 from 'web3'
+import web3_contract from 'web3-eth-contract'
+import net from 'net'
+const { Contract, ContractSendMethod, Options } = web3_contract
+//import HDWalletProvider from "truffle-hdwallet-provider"
+//import { Contract, ContractSendMethod, Options } from 'web3-eth-contract'
+
+/**
+ * Deploy the given contract
+ * @param {string} contractName name of the contract to deploy
+ * @param {Array<any>} args list of constructor' parameters
+ * @param {string} from account used to send the transaction
+ * @param {number} gas gas limit
+ * @return {Options} deployed contract
+ */
+export const deploy = async (contractName, args, url, from, gas) => {
+  console.log('deploying', contractName, 'with args', args, 'from', from, 'gas', gas)
+
+  // if url starts with http, use http provider else use ipc provider
+  var web3
+  if (url.startsWith('http')) {
+    web3 = new Web3(new Web3.providers.HttpProvider(url))
+  } else {
+    web3 = new Web3(new Web3.providers.IpcProvider(url, net))
+  }
+  console.log(`deploying ${contractName}`)
+
+  // Load contract from builds folder using grep to find the right file with contract name
+  const abiPath = fs.readdirSync('./builds').filter(file => file.match(new RegExp(`${contractName}.abi`)))
+
+  //const abiPath = `./builds/contracts_${contractName}_sol_${contractName}.abi`
+  const abi = JSON.parse(fs.readFileSync("./builds/" + abiPath[0], 'utf8'))
+  //const bytecodePath = `./builds/contracts_${contractName}_sol_${contractName}.bin`
+  const bytecodePath = fs.readdirSync('./builds').filter(file => file.match(new RegExp(`${contractName}.bin`)))
+  const bytecode = fs.readFileSync("./builds/" + bytecodePath[0], 'utf8')
+  console.log('bytecode', bytecode)
+
+  const contract = new web3.eth.Contract(abi)
+  console.log('contract', contract)
+  const accounts = await web3.eth.getAccounts()
+  console.log('accounts', accounts)
+
+  const contractSend = contract.deploy({
+    data: bytecode,
+    arguments: args
+  })
+
+  const newContractInstance = await contractSend.send({
+    from: from || accounts[0],
+    gas: gas || 300000000,
+  })
+
+  return newContractInstance.options
+}
+
